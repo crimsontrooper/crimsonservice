03be5dc50701623e4a20eff6ff0c2658fdbc80c0
lorenzosantini05 <105849734+lorenzosantini05@users.noreply.github.com>
2024-10-05 10:54:38 +0200

    Commit2

--- index.ts
+++ index.ts
@@ -12,8 +12,8 @@ const [cert, key] = await Promise.all([
 
 const server = https.createServer({ key, cert }, app);
 let port:any = process.env.PORT ? parseInt(process.env.PORT) : 3000
-var host = process.env.PORT ? "0.0.0.0" : "localhost"
-server.listen(port, host, () => console.log("Server Avviato sulla porta " + 3000));
+var host = process.env.PORT ? "https://crimsonservice.onrender.com" : "localhost"
+server.listen(port, host, () => console.log("Server Avviato sulla porta " + port));
 
 app.use("/", (req : Request, res : Response, next : NextFunction) => {
     console.log(`>--> ${req.method} ${req.url}`)
